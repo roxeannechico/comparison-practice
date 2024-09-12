d3613e7e35a17603b17fc0c688f687118d5d1b68
roxeannechico <roseannechico22@gmail.com>
2024-09-12 21:55:54 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let firstFavNumb = 22;
+let secondFavNumb = 9;
+
+//Use comparison operator and store the results in variables
+let isfirstFavNumbGreaterThansecondFavNumb = firstFavNumb > secondFavNumb;
+let isfirstFavNumbLessThansecondFavNumb = firstFavNumb < secondFavNumb;
+let isfirstFavNumbGreaterThanOrEqualTosecondFavNumb = firstFavNumb >= secondFavNumb;
+let isfirstFavNumbLessThanOrEqualTosecondFavNumb = firstFavNumb <= secondFavNumb;
+let isfirstFavNumbStrictEqualitysecondFavNumb = firstFavNumb === secondFavNumb;
+let isfirstFavNumbLooseEqualitysecondFavNumb = firstFavNumb == secondFavNumb;
+let isfirstFavNumbStrictInequalitysecondFavNumb = firstFavNumb !== secondFavNumb;
+let isfirstFavNumbLooseInequalitysecondFavNumb = firstFavNumb != secondFavNumb;
+let isfirstFavNumbAndsecondFavNumb = firstFavNumb>0 && secondFavNumb>0;
+let isfirstFavNumbOrsecondFavNumb = firstFavNumb>0 || secondFavNumb>0;
+let isfirstFavNumbNotsecondFavNumb = !firstFavNumb > secondFavNumb;
+
+console.log("is firstFavNumb greater than secondFavNumb?", isfirstFavNumbGreaterThansecondFavNumb);
+console.log("is firstFavNumb less than secondFavNumb?", isfirstFavNumbLessThansecondFavNumb);
+console.log("is firstFavNumb greater than or equal to secondFavNumb?", isfirstFavNumbGreaterThanOrEqualTosecondFavNumb);
+console.log("is firstFavNumb less than or equal to secondFavNumb?", isfirstFavNumbLessThanOrEqualTosecondFavNumb);
+console.log("is firstFavNumb Strict Equality secondFavNumb?", isfirstFavNumbStrictEqualitysecondFavNumb);
+console.log("is firstFavNumb Loose Equality secondFavNumb?", isfirstFavNumbLooseEqualitysecondFavNumb);
+console.log("is firstFavNumb Strict Inequality secondFavNumb?", isfirstFavNumbStrictInequalitysecondFavNumb);
+console.log("is firstFavNumb Loose Inequality secondFavNumb?", isfirstFavNumbLooseInequalitysecondFavNumb);
+console.log("is firstFavNumb And secondFavNumb?", isfirstFavNumbAndsecondFavNumb);
+console.log("is firstFavNumb Or secondFavNumb?", isfirstFavNumbOrsecondFavNumb);
+console.log("is firstFavNumb Not secondFavNumb?", isfirstFavNumbNotsecondFavNumb);
